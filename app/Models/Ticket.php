21992c258df78afb769a0aa9420bd530c735c4e0
Vijahat <vijahat@topsinfosolutions.com>
2022-06-20 13:46:31 +0530

    update model add relation with user

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Ticket extends Model
 {
@@ -29,4 +30,9 @@ class Ticket extends Model
         'lastmodified_by_type',
         'lastmodified_by',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
 }

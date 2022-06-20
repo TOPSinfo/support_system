767d755a7d2714103d5c3d19fba5273b54232fdd
Vijahat <vijahat@topsinfosolutions.com>
2022-06-20 13:48:04 +0530

    user seeder added

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data[] = array(
+            'name' => 'Vijahat',
+            'email' => 'vijahat@topsinfosolutions.com',
+            'password' => Hash::make('password'),
+        );
+        $data[] = array(
+            'name' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => Hash::make('password'),
+        );
+        $data[] = array(
+            'name' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => Hash::make('password'),
+        );
+        $data[] = array(
+            'name' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => Hash::make('password'),
+        );
+        $data[] = array(
+            'name' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => Hash::make('password'),
+        );
+        DB::table('users')->insert($data);
+    }
+}

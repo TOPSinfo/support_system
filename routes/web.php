04a98a3954d82f019e1114ccb7d89c3309723ed2
Vijahat <vijahat@topsinfosolutions.com>
2022-06-20 15:11:16 +0530

    admin LTE setup done

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,16 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('admin', function () {
+    return view('admin');
+});
+
+Route::prefix('ticket')->group(function () {
+    Route::get('/list', [App\Http\Controllers\TicketController::class, 'listTicket'])->name('ticket.list');
+    Route::get('/add', [App\Http\Controllers\TicketController::class, 'createTicket'])->name('ticket.add');
+    Route::post('/save', [App\Http\Controllers\TicketController::class, 'saveTicket'])->name('ticket.save');
+    Route::get('/edit/{id}', [App\Http\Controllers\TicketController::class, 'editTicket'])->name('ticket.edit');
+    Route::post('/update', [App\Http\Controllers\TicketController::class, 'updateTicket'])->name('ticket.update');
+});
+
